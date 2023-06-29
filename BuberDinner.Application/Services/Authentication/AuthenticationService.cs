355ff63c4b0e9e490dd699560cd77b50020b7aab
Pedro Tentugal <pedro.tentugal@amd.com>
2023-06-29 16:11:31 +0100

    Finished first part

---
+++ BuberDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -0,0 +1,26 @@
+﻿namespace BuberDinner.Application.Services.Authentication;
+
+public class AuthenticationService : IAuthenticationService
+{
+    public AuthenticationResult Register(string firstname, string lastName, string email, string password)
+    {
+        return new AuthenticationResult(
+            Guid.NewGuid(),
+            firstname,
+            lastName,
+            email,
+            "token"
+        );
+    }
+
+    public AuthenticationResult Login(string email, string password)
+    {
+        return new AuthenticationResult(
+            Guid.NewGuid(),
+            "firstname",
+            "lastName",
+            email,
+            "token"
+        );
+    }
+}
